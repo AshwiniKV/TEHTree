392308283f4a9d8f4b5419738223ffcefac379e4
AshwiniKV <ashwinikv01@gmail.com>
2021-07-21 19:19:05 +0100

    Upload the BLS dataset

--- tests/Illustration/illustration.R
+++ tests/Illustration/illustration.R
@@ -1,36 +1,15 @@
 
-setwd("./Desktop/TEHTrees_code")
 # Request access to the BLS dataset (6 months)
 blsdata6<-read.csv("blsdata6.csv")
 blsdata6<-blsdata6[,-1]
+names(blsdata6)<-c("trt", "bmi", "kcal24h6", "age", "edeq14", "hunger")
 
-redbls<-blsdata6[,c(3, 10, 15, 53, 55, 84, 102, 106, 119:129, 132, 168, 172:174, 257:258, 273)]
-redbls<-redbls[complete.cases(redbls),]
-
-redbls<-redbls[!redbls$trt == "Control",]
-
-Y<-redbls$kcal24h6
-Z<-redbls$trt
-Z<-factor(Z)
-levels(Z)<-c("Control", "Trt", "Trt")
-levels(Z)<-c("FALSE", "TRUE")
-redbls$trt<-Z
-
-indx <- sapply(redbls, is.factor)
-redbls[indx] <- lapply(redbls[indx], function(x) as.numeric(x))
-
-# Subset of covariates
-
-redbls2<-blsdata6[,c(3, 15, 84, 168, 122, 257:258, 174, 273)]
-redbls2<-redbls[,c(1, 3, 6, 21, 12, 25:26, 24, 27)]
-###############
 # Causal Tree - double sample
 # Use only 4 covariates - age, bmi, hunger and edeq14.0
 
 library(causalTree)
-redbls1<-redbls2[,-1]
-redbls1<-redbls1[,-c(5:6, 8)]
-tree <- causalTree(kcal24h6~., data = redbls1, treatment = as.logical(Z),
+redbls1<-blsdata6[,-c(1)]
+tree <- causalTree(kcal24h6~., data = redbls1, treatment = (blsdata6$trt -1),
                    split.Rule = "CT", cv.option = "CT", split.Honest = T, cv.Honest = T, split.Bucket = F,
                    xval = 6, cp = 0, minsize = 10)
 
@@ -45,31 +24,30 @@ rpart.plot(opfit)
 #set.seed(15)
 set.seed(60)
 library(TEHTree)
-X<-redbls2[,c(2, 4:5, 8)]
-matched1_orig <- MatchForTree(Y=Y, Z=as.logical(Z), X=X, version="prognostic")
-LT1<-LTfunction(Y, as.logical(Z), X, X)
+X<-blsdata6[,c(2, 4:6)]
+matched1_orig <- MatchForTree(Y=blsdata6$kcal24h6, Z=blsdata6$trt-1, X=X, version="prognostic")
+LT1<-LTfunction(blsdata6$kcal24h6, blsdata6$trt-1, X, X)
 TreeMat(LT1, matched1_orig$Y.match, as.matrix(matched1_orig$X.match))
 
 ##################################################
 
 # Permuted
 library(permute)
-set.seed(250)
+set.seed(50)
 ids_X<-1:nrow(X)
 newdata<-data.frame(cbind(ids_X, X))
 nids_X<-shuffle(newdata$ids_X)
 newX<-newdata[nids_X,-1]
 
-matched2_orig <- MatchForTree(Y=Y, Z=as.logical(Z), X=newX, version="prognostic")
-LT2<-LTfunction(Y, as.logical(Z), newX, newX)
+matched2_orig <- MatchForTree(Y=blsdata6$kcal24h6, Z=blsdata6$trt-1, X=newX, version="prognostic")
+LT2<-LTfunction(blsdata6$kcal24h6, blsdata6$trt-1, newX, newX)
 TreeMat(LT2, matched2_orig$Y.match, as.matrix(matched2_orig$X.match))
 #####################################
 
 library(causalTree)
-
-newdata1<-data.frame(cbind(Y, newX))
+newdata1<-data.frame(cbind(blsdata6$kcal24h6, newX))
 names(newdata1)<-c("Y", "bmi0", "age", "edeq14.0", "hunger")
-tree <- causalTree(Y~., data = newdata1, treatment = as.logical(Z),
+tree <- causalTree(Y~., data = newdata1, treatment = blsdata6$trt-1,
                    split.Rule = "CT", cv.option = "CT", split.Honest = T, cv.Honest = T, split.Bucket = F,
                    xval = 6, cp = 0, minsize = 10)
 
@@ -81,14 +59,14 @@ rpart.plot(opfit)
 # Causal Forest
 ###############
 set.seed(100)
-cases <- sample(seq_len(nrow(redbls2)), round(nrow(redbls2) * .67))
-train <- redbls2[cases,]
-test <- redbls2[-cases,]
+cases <- sample(seq_len(nrow(blsdata6)), round(nrow(blsdata6) * .67))
+train <- blsdata6[cases,]
+test <- blsdata6[-cases,]
 
 library(grf)
 
 cf <- causal_forest(
-  X = model.matrix(~ ., data = train[, c(2, 4:5, 8)]),
+  X = model.matrix(~ ., data = train[, c(2, 4:6)]),
   Y = train$kcal24h6,
   W = as.numeric(train$trt)-1,
   num.trees = 15000,
@@ -97,7 +75,7 @@ cf <- causal_forest(
 
 preds <- predict(
   object = cf,
-  newdata = model.matrix(~ ., data = test[, c(2, 4:5, 8)]),
+  newdata = model.matrix(~ ., data = test[, c(2, 4:6)]),
   estimate.variance = TRUE
 )
 
